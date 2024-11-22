8f0409afcd3f325ed93714d3575a78591ed4f918
Gabeperson <95374687+Gabeperson@users.noreply.github.com>
2024-11-22 14:40:47 -0800

    relative base?

--- sm213_editor/vite.config.js
+++ sm213_editor/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 export default defineConfig({
-    base: "/sm213_editor",
+    // base: "/sm213_editor",
+    base: "./",
     esbuild: {
         supported: {
             "top-level-await": true, //browsers can handle top-level-await features
