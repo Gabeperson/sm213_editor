efd08146bc66c781365f03d0f1b1da2f0af8fb36
Gabeperson <95374687+Gabeperson@users.noreply.github.com>
2024-11-22 14:40:00 -0800

    fix url base issue

--- sm213_editor/vite.config.js
+++ sm213_editor/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 export default defineConfig({
+    base: "/sm213_editor",
     esbuild: {
         supported: {
             "top-level-await": true, //browsers can handle top-level-await features
